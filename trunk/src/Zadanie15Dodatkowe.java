9fca3018bc47b28fdfa9924bedf3aa3aa04aaf88
s8376@pjwstk.edu.pl <s8376@pjwstk.edu.pl@2c044fcf-a0ce-7996-88eb-45ac795e61ee>
2010-11-04 12:58:54 +0000

    Zadania

---
+++ trunk/src/Zadanie15Dodatkowe.java
@@ -0,0 +1,67 @@
+import javax.swing.JOptionPane;
+
+/**
+ * Zadanie 15 dodatkowe
+ * 
+ * Zalozenia: piec liczb - a, b, c, d, e posortowac za pomoca jak najmniejszej
+ * ilosci testow
+ * 
+ * @author s8376
+ */
+public class Zadanie15Dodatkowe {
+	public static void main(String[] args) {
+		int numOfNumbers = 5;
+		int[] numbers = new int[numOfNumbers];
+		int x;
+
+		Zadanie15Dodatkowe handler = new Zadanie15Dodatkowe();
+		for (x = 0; x < numOfNumbers; ++x) {
+			numbers[x] = handler.handle(x + 1); 
+		}
+		
+		boolean flagChanges = false;
+		int buffer;
+		do {
+			flagChanges = false;
+			for (x = 0; x < numOfNumbers; ++x) {
+				if (x < (numOfNumbers - 1) && numbers[x] > numbers[x + 1]) {
+					buffer = numbers[x];
+					numbers[x] = numbers[x + 1];
+					numbers[x + 1] = buffer;
+					flagChanges = true;					
+				}				
+			}
+		} while (flagChanges == true);
+			
+		System.out.println("Wynik:");
+		for (x = 0; x < numOfNumbers; ++x) {
+			System.out.print("" + numbers[x] + ", ");
+		}
+	}
+
+	/**
+	 * Zwraca liczbe wprowadzona przez uzytkownika
+	 * 
+	 * @param int x numer wprowadzanej liczby
+	 * @return int wprowadzona liczba
+	 */
+	protected int handle(int x) {
+		boolean flag = false;
+		int number = 0;
+
+		do {
+			flag = false;
+			String dane = JOptionPane.showInputDialog("Podaj liczbę nr." + x);
+			try {
+				number = Integer.parseInt(dane);
+				flag = true;
+			} catch (NumberFormatException e) {
+				JOptionPane.showMessageDialog(null,
+						"Podana wartość nie jest liczbą!");
+				flag = false;
+			}
+		} while (flag == false);
+
+		return number;
+	}
+}
---
+++ trunk/src/Zadanie8.java
@@ -0,0 +1,25 @@
+/**
+ * Zadanie 8
+ * 
+ * Napisać program, który dla 2 liczb naturalnych podanych w programie,
+ * wyprowadza ich sumę, różnicę, iloczyn, iloraz, resztę z dzielenia, wynik
+ * dzielenia całkowitego z opisem, np. x = wartość; y = wartość; x+y = wartość
+ * sumy x-y = wartość różnicy x*y = wartość iloczynu x:y = wartość ilorazu x%y =
+ * reszta z dzielenia x/y = wynik dzielenia całkowitego
+ * 
+ * @author s8376
+ */
+public class Zadanie8 {
+	public static void main(String[] args) {
+		int x = 4;
+		int y = 2;
+
+		System.out.println("x = wartość: " + x + "\n" + "y = wartość: " + y
+				+ "\n" + "x+y = wartość sumy: " + (x + y) + "\n"
+				+ "x-y = wartość rónicy: " + (x - y) + "\n"
+				+ "x*y = wartość iloczynu: " + (x * y) + "\n"
+				+ "x:y = wartość ilorazu: " + ((double) x / (double) y) + "\n"
+				+ "x%y = reszta z dzielenia: " + (x % y) + "\n"
+				+ "x/y = wynik dzielenia całkowitego: " + (x / y));
+	}
+}

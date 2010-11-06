6ea0b8daa98454c3c829e9ee5ab344d9ab3f8044
s8376 <s8376@pjwstk.edu.pl>
2010-11-06 09:31:42 +0000

    Labolatorium

--- trunk/src/Zadanie16.java
+++ trunk/src/Zadanie16.java
@@ -1,4 +1,16 @@
-
+/**
+ * Zadanie 16
+ * 
+ * Napisać program, który pobiera 1 znak i wyprowadza wartość liczbową
+ * odpowiadającą cyfrze szesnastkowej podanej w postaci tego znaku lub -1 jeśli
+ * dany znak nie odpowiada żadnej cyfrze szesnastkowej. Np: '0' -> 0, 'A' -> 10,
+ * 'a'-> 10, 'x' -> -1.
+ * 
+ * @author s8376
+ * @version $Revision$
+ */
 public class Zadanie16 {
+	public static void main(String[] args) {
 
+	}
 }

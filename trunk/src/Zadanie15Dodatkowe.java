d7789ce793ca1b2e0b2f86c64c5a0a4e3161db2c
s8376@pjwstk.edu.pl <s8376@pjwstk.edu.pl@2c044fcf-a0ce-7996-88eb-45ac795e61ee>
2010-11-09 14:55:19 +0000

    Zadanie 15d prace

--- trunk/src/Zadanie15Dodatkowe.java
+++ trunk/src/Zadanie15Dodatkowe.java
@@ -18,10 +18,68 @@ public class Zadanie15Dodatkowe {
 		int c = 9;
 		int d = 2;
 		int e = 0;
-		int buffer;
-
 		
+		int aCount = 0;
+		int bCount = 0;
+		int cCount = 0;
+		int dCount = 0;
+		int eCount = 0;
 		
+		if (a < b) {
+			++bCount;
+		} else {
+			++aCount;
+		}
+		if (a < c) {
+			++cCount;
+		} else {
+			++aCount;
+		}
+		if (a < d) {
+			++dCount;
+		} else {
+			++aCount;
+		}
+		if (a < e) {
+			++eCount;
+		} else {
+			++aCount;
+		}
+		
+		if (b < c) {
+			++cCount;
+		} else {
+			++bCount;
+		}
+		if (b < d) {
+			++dCount;
+		} else {
+			++bCount;
+		}
+		if (b < e) {
+			++eCount;
+		} else {
+			++bCount;
+		}
+		
+		if (c < d) {
+			++dCount;
+		} else {
+			++cCount;
+		}
+		if (c < e) {
+			++eCount;
+		} else {
+			++cCount;
+		}
+		
+		if (d < e) {
+			++eCount;
+		} else {
+			++dCount;
+		}
+		
+		System.out.println("Count: a" + aCount + ", b" + bCount + ", c" + cCount + ", d" + dCount + ", e" + eCount);
 		System.out.println("Wynik: a:" + a + "; b:" + b + "; c:" + c + "; d:" + d + "; e:" + e);
 	}
 }

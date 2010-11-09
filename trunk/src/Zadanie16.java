293c85482e5b722360a938380a20919c7996802c
s8376@pjwstk.edu.pl <s8376@pjwstk.edu.pl@2c044fcf-a0ce-7996-88eb-45ac795e61ee>
2010-11-09 06:25:15 +0000

    Zadanie 16

--- trunk/src/Zadanie16.java
+++ trunk/src/Zadanie16.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Zadanie 16
  * 
@@ -11,6 +15,29 @@
  */
 public class Zadanie16 {
 	public static void main(String[] args) {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		String inputString = "";
+//		System.out.println("Podaj ciąg: ");
+//		try {
+//			inputString = br.readLine();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
 
+		String inputString = "abcdefg0123456789ABCDEFG";
+
+		int status;
+		int stringLenght = inputString.length();
+		for (int x = 0; x < stringLenght; ++x) {
+			status = -1;
+			if (inputString.charAt(x) >= 48 && inputString.charAt(x) <= 57) { // 0-9
+				status = inputString.charAt(x) - 48;
+			} else if (inputString.charAt(x) >= 65 && inputString.charAt(x) <= 70) { // A-F
+				status = (inputString.charAt(x) - 65) + 10;
+			} else if (inputString.charAt(x) >= 97 && inputString.charAt(x) <= 102) { // a-f
+				status = (inputString.charAt(x) - 97) + 10;
+			}
+			System.out.print("'" + inputString.charAt(x) + "' -> " + status + ", ");
+		}
 	}
 }

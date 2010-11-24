cb5b6235c3660c7afa2a553b52f4f19915841467
s8376@pjwstk.edu.pl <s8376@pjwstk.edu.pl@2c044fcf-a0ce-7996-88eb-45ac795e61ee>
2010-11-24 20:55:27 +0000

    Package Shool

---
+++ trunk/src/school/Point.java
@@ -0,0 +1,65 @@
+package school;
+
+/**
+ * Point
+ * 
+ * @author s8376
+ * @version $Revision$
+ */
+public class Point {
+	protected int _x;
+	protected int _y;
+
+	public Point() {
+		_x = 0;
+		_y = 0;
+	}
+
+	public Point(int a) {
+		_x = a;
+		_y = 0;
+	}
+
+	public Point(int a, int b) {
+		_x = a;
+		_y = b;
+	}
+
+	public double distance(Point p) {
+		return Math.sqrt((p.getX() - this.getX()) * (p.getX() - this.getX())
+				+ (p.getY() - this.getY()) * (p.getY() - this.getY()));
+	}
+
+	public int getX() {
+		return _x;
+	}
+
+	public int getY() {
+		return _y;
+	}
+
+	public boolean parallel(Point p) {
+		if ((this.getX() == p.getX() && this.getY() != p.getY())
+				|| (this.getX() != p.getX() && this.getY() == p.getY())) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void setX(int a) {
+		_x = a;
+	}
+
+	public void setY(int b) {
+		_y = b;
+	}
+
+	public void show() {
+		System.out.println(this.toString());
+	}
+
+	public String toString() {
+		return ("Punkt x: " + _x + "; y:" + _y);
+	}
+}

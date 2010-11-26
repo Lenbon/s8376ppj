9df84d8b83bd8c0304308fb9d8395c29f2af3de5
s8376 <s8376@pjwstk.edu.pl>
2010-11-26 18:43:59 +0000

    Zadanie 25

--- trunk/src/Zadanie25.java
+++ trunk/src/Zadanie25.java
@@ -19,24 +19,24 @@ import school.Point;
  */
 public class Zadanie25 {
 	public static void main(String[] args) {
-		Circle c1 = new Circle(new Point(1, 1), 5);// FIXME
-		System.out.println("To jest koło nr." + c1.dajNr() + " z ilości " + c1.dajIlosc());
+		Circle c1 = new Circle(new Point(1, 1), 5);
+		System.out.println("To jest koło nr." + c1.dajNr() + " z ilości " + Circle.dajIlosc());
 		
 		Circle c2 = new Circle(new Point(2, 2), 5);
-		System.out.println("To jest koło nr." + c2.dajNr() + " z ilości " + c2.dajIlosc());
+		System.out.println("To jest koło nr." + c2.dajNr() + " z ilości " + Circle.dajIlosc());
 		
 		Circle c3 = new Circle(new Point(3, 3), 5);
-		System.out.println("To jest koło nr." + c3.dajNr() + " z ilości " + c3.dajIlosc());
+		System.out.println("To jest koło nr." + c3.dajNr() + " z ilości " + Circle.dajIlosc());
 		
 		Circle c4 = new Circle(new Point(4, 4), 5);
-		System.out.println("To jest koło nr." + c4.dajNr() + " z ilości " + c4.dajIlosc());
+		System.out.println("To jest koło nr." + c4.dajNr() + " z ilości " + Circle.dajIlosc());
 		
 		Circle c5 = new Circle(new Point(5, 5), 5);
-		System.out.println("To jest koło nr." + c5.dajNr() + " z ilości " + c5.dajIlosc());
+		System.out.println("To jest koło nr." + c5.dajNr() + " z ilości " + Circle.dajIlosc());
 		
 		System.out.println("------");
 		
 		// powtorne odpytanie kola nr. 3
-		System.out.println("To jest koło nr." + c3.dajNr() + " z ilości " + c3.dajIlosc());
+		System.out.println("To jest koło nr." + c3.dajNr() + " z ilości " + Circle.dajIlosc());
 	}
 }

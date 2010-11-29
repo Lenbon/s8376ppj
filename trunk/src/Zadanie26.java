2f2446d55f630c1af8d588780830465febde8d7f
s8376@pjwstk.edu.pl <s8376@pjwstk.edu.pl@2c044fcf-a0ce-7996-88eb-45ac795e61ee>
2010-11-29 22:47:06 +0000

    Zadanie 26

--- trunk/src/Zadanie26.java
+++ trunk/src/Zadanie26.java
@@ -61,32 +61,32 @@ public class Zadanie26 {
 		try {
 			Fraction f1 = new Fraction(2, 5), f2 = new Fraction(3), f3 = new Fraction(4, 0);
 
-//			f1.show(); // 2/5 = 0.4
-//			f2.show(); // 3/10 = 0.3
-//			f3.show(); // 4/0 = Error
+			f1.show(); // 2/5 = 0.4
+			f2.show(); // 3/10 = 0.3
+			f3.show(); // 4/0 = Error
 
 			f1.setNumerator(15);
-//			f1.show(); // 15/5 = 3
+			f1.show(); // 15/5 = 3
 
 			f2.setDenominator(-2);
-//			f2.show(); // 3/-2 = -(1+1/2) = -1.5 // FIXME
+			f2.show(); // 3/-2 = -(1+1/2) = -1.5 // FIXME
 
 			f3.setNumerator(20);
 			f3.setDenominator(4);
-//			f3.show(); // 20/4 = 5
+			f3.show(); // 20/4 = 5
 
-//			f2.setDenominator(2);
-//			f3.greater(f2).show(); // 20/4 = 5
+			f2.setDenominator(2);
+			f3.greater(f2).show(); // 20/4 = 5
 
 			f3.simplify().show(); // 20/4 = 5
 
-//			f3.setDenominator(12);
-//			f3.simplify().show(); // 5/3 = 1+2/3
+			f3.setDenominator(12);
+			f3.simplify().show(); // 5/3 = 1+2/3
 
-			// f1.add(f2).show(); // 45/10 = 4+5/10 = 4+1/2 = 4.5 // FIXME
-			// f1.sub(f2).show(); // 15/10 = 1+ 5/10 = 1.5 // FIXME
-			// f1.mult(f2).show(); // 45/10 = 4+5/10 = 4+1/2 = 4.5 // FIXME
-			// f1.div(f2).show(); // 30/15 = 2 // FIXME
+			f1.add(f2).show(); // 45/10 = 4+5/10 = 4+1/2 = 4.5
+			f1.sub(f2).show(); // 15/10 = 1+ 5/10 = 1.5
+			f1.mult(f2).show(); // 45/10 = 4+5/10 = 4+1/2 = 4.5
+			f1.div(f2).show(); // 30/15 = 2
 		} catch (Exception e) {
 			e.getStackTrace();
 		}

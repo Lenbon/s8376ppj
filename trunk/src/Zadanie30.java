35db288001f52964b3db51df6d9711882dbddbd9
s8376@pjwstk.edu.pl <s8376@pjwstk.edu.pl@2c044fcf-a0ce-7996-88eb-45ac795e61ee>
2010-12-18 06:36:33 +0000

    Zadanie 30, notki

--- trunk/src/Zadanie30.java
+++ trunk/src/Zadanie30.java
@@ -1,81 +1,46 @@
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.TreeMap;
-import java.util.TreeSet;
 
+import javax.swing.JOptionPane;
 
+/**
+ * Zadanie 30
+ * 
+ * Napisać program sprawdzający, czy dana relacja w zbiorze X = {1, 2, 3, ..., n},
+ * gdzie n jest liczbą naturalną  podaną przez użytkownika, jest:
+ * *    zwrotna,
+ * *    symetryczna.
+ * Użytkownik podaje relację w postaci zbioru par liczb, np.: r = {(1,2), (2,1),
+ * (2,2), (1,1), (1,3)} dla n = 3.
+ * 
+ * @author s8376
+ * @version $Revision$
+ */
 public class Zadanie30 {
-//	relacja zwrotna: tablica musi zawierać wszystkie pary : x,x
-//	relacja symetryczna: musi zawierac pary : x,y i y,x
-	
-	
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-	
-		// Pojęcia kolekcja:
-		// lista - rozwinięcie tablicy
-		// zbiór - lista unikalnych elementów bez indeksów
-		// mapa - zbiór par: klucz wartość; klucz może być dowolnego typu
+		boolean flag = false;
+		int n = 0;
 		
-		ArrayList list = new ArrayList();
+		do {
+			try {
+				n = Integer.parseInt(JOptionPane.showInputDialog("Podaj liczbę n:"));
+				if (n <=0) {
+					throw new Exception("n musi być większe od 0");
+				}
+				flag = true;
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(null, "n nie jest liczbą większą od 0");
+				flag = false;
+			}			
+		} while (flag == false);
 		
-		
-//		list.
-		
-		list.add(12);
-		list.add(13);
-		list.add("22");
-		list.add(14);
-		list.add(15);
-		
-		
-		
-		
-		for (Object e: list) {
-			if (e instanceof Integer) {
-				System.out.println(e);
-			} else {
-				System.out.println("ERROR1");
-			}
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for (int x = 1; x <= n; ++x) {
+			list.add(x);
 		}
-		
-		HashSet<String> zbior = new HashSet<String>();
-		zbior.add("Ala");
-		zbior.add("Ma");
-		zbior.add("Kota");
-		zbior.add("Kota");
-		
-		for (Object val : zbior) {
-			System.out.println(val);
+
+		while (true) {
+			
 		}
-		
-		TreeSet tree = new TreeSet();
-		tree.add(123);
-		
-		
-		// mapa
-		HashMap<Integer, String> map = new HashMap<Integer, String>();
-		map.put(1, "Pierwszy");
-		map.put(2, "Drugi");
-		map.put(3, "Trzeci");
-		map.put(2, "Czwarty(nadpisuje Drugi)");
-		
-//		map.
-		
-		System.out.println(map.get(2));
-		
-		TreeMap<String, String> treeMap = new TreeMap<String, String>();
-		treeMap.put("Ala", "Różowy");
-		treeMap.put("Kamila", "Żółty");
-		treeMap.put("Beata", "Niebieski");
-		treeMap.put("Ilona", "Zielony");
-		
-//		treeMap.
-		
 		
 	}
-	
 }

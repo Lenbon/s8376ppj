d22dd04d0b91be00e530fed1b5d95ab4bc1e3949
s8376@pjwstk.edu.pl <s8376@pjwstk.edu.pl@2c044fcf-a0ce-7996-88eb-45ac795e61ee>
2011-01-24 05:25:39 +0000

    Zadania

---
+++ trunk/src/Zadanie43.java
@@ -0,0 +1,18 @@
+/**
+ * Zadanie 43
+ * 
+ * Leksemy to ciągi znaków rozdzielone znakami spacji.
+ * 
+ * Napisz program, który kopiuje z pliku input.txt do output1.txt wszystkie
+ * leksemy będące liczbami całkowitymi (typu int) i podaje ich sumę na konsolę.
+ * Następnie program zapisuje pozostałe leksemy wraz z liczbą wystąpień każdego
+ * z nich do pliku output2.txt.
+ *  
+ * @author s8376
+ * @version $Revision$
+ */
+public class Zadanie43 {
+	public static void main(String[] args) {
+		
+	}
+}

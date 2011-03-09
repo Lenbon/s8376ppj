253e68206313d912e5efd6e8f44ac36fe232f0d9
s8376 <s8376@pjwstk.edu.pl>
2011-03-09 19:51:18 +0000

    Zadanie01_3

--- branches/wdrabik/src/Zadanie01_3.java
+++ branches/wdrabik/src/Zadanie01_3.java
@@ -25,69 +25,79 @@ import javax.swing.JOptionPane;
  * @version $Revision$
  */
 public class Zadanie01_3 {
-	public static Integer[] sort(Integer[] array) {
+	public static int[] sort(int[] array) {
+		boolean changeFlag = true;
+		int buffer;
+
+		while (changeFlag == true) {
+			changeFlag = false;
+			for (int x = 0; x < array.length - 1; x++) {
+				if (array[x] > array[x + 1]) {
+					buffer = array[x];
+					array[x] = array[x + 1];
+					array[x + 1] = buffer;
+					changeFlag = true;
+				}
+			}
+		}
+		
 		return array;
 	}
 	public static void main(String[] args) {
-		Integer[] user = new Integer[6];
-		Integer[] machine = new Integer[6];
+		int[] user = new int[6];
+		int[] machine = new int[6];
 
-//		Integer inputInteger;
-//		String input, message = "";
-//		String[] array;
-//		boolean inputFlag = false;
-//		
-//		while (inputFlag == false) {
-//			try {
-//				input = JOptionPane.showInputDialog((message.length() > 0 ? message + "\n" : "") + "Podaj ciąg znaków:");
-//				if (input == null) {
-//					System.exit(0);
-//				}
-//				array = input.split(" ");
-//			
-//				if (array.length != 6) {
-//					throw new Exception("Podaj dokładnie 6 liczb");
-//				}
-//				for (int x = 0; x < array.length; x++) {
-//					try {
-//						inputInteger = Integer.parseInt(array[x]);
-//						if (inputInteger < 1 || inputInteger > 49) {
-//							throw new Exception("Podane liczby muszą być z zakresu 1 - 49 (podano " + inputInteger + ")");	
-//						}
-//						for (Integer val : user) {
-//							if (val.equals(input)) {
-//								throw new Exception("Zdublowana liczba " + inputInteger);		
-//							}
-//						}
-//						user[x] = inputInteger;
-//					} catch (NumberFormatException e) {
-//						throw new Exception("Nieprawidłowy format danych wejściowych.\nSpróbuj: 1 2 3 4 5 6");
-//					}
-//				}
-//				
-//				inputFlag = true;
-//			} catch (Exception e) {
-//				message = e.getMessage();
-//				inputFlag = false;
-//			}
-//		}
-//		
-//		user = sort(user);
+		int inputInteger;
+		String input, message = "";
+		String[] array;
+		boolean inputFlag = false;
 		
-		
-		
-		System.out.println((1 + (int) Math.random() * 48));
-		System.exit(0);
-		
+		while (inputFlag == false) {
+			try {
+				input = JOptionPane.showInputDialog((message.length() > 0 ? message + "\n" : "") + "Podaj ciąg znaków:");
+				if (input == null) {
+					System.exit(0);
+				}
+				array = input.split("[ ,;]+");
+			
+				if (array.length != 6) {
+					throw new Exception("Podaj dokładnie 6 liczb");
+				}
+				for (int x = 0; x < array.length; x++) {
+					try {
+						inputInteger = Integer.parseInt(array[x]);
+						if (inputInteger < 1 || inputInteger > 49) {
+							throw new Exception("Podane liczby muszą być z zakresu 1 - 49 (podano " + inputInteger + ")");	
+						}
+						for (int val : user) {
+							if (val == inputInteger) {
+								throw new Exception("Zdublowana liczba " + inputInteger);		
+							}
+						}
+						user[x] = inputInteger;
+					} catch (NumberFormatException e) {
+						throw new Exception("Nieprawidłowy format danych wejściowych.\nSpróbuj: 1 2 3 4 5 6");
+					}
+				}
+				
+				inputFlag = true;
+			} catch (Exception e) {
+				message = e.getMessage();
+				inputFlag = false;
+			}
+		}
+
+		user = sort(user);
+
 		boolean randomFlag;
 		int machineItems = 0, random;
 
 		while (machineItems < 6) {
 			randomFlag = true;
-			random = (1 + (int) Math.random() * 48);
+			random = 1 + (int) (Math.random() * 48);
 
-			for (Integer val : user) {
-				if (val.equals(random)) {
+			for (int val : machine) {
+				if (val == random) {
 					randomFlag = false;
 					break;
 				}
@@ -98,18 +108,34 @@ public class Zadanie01_3 {
 			}
 		}
 
-//		machine = sort(machine);
+		machine = sort(machine);
 		
-//		System.out.println("Liczby podane przez użytkownika:");
+		
+		// wyswietlenie wynikow
+		
+		
+		System.out.println("Liczby podane przez użytkownika:");
 
-//		for (Integer val : user) {
-//			System.out.print(" " + val);
-//		}
-
-		System.out.println("\nLiczby podane przez użytkownika:");
-
-		for (Integer val : machine) {
+		for (Integer val : user) {
 			System.out.print(" " + val);
 		}
+
+		System.out.println("\nLiczby wylosowane:");
+
+		String hitsList = "";
+		int hits = 0;
+		
+		for (int val : machine) {
+			System.out.print(" " + val);
+			
+			for (int subVal : user) {
+				if (val == subVal) {
+					hitsList += " " + subVal;
+					hits++;
+				}
+			}
+		}
+		
+		System.out.println("\nLiczba trafień: " + hits + "\n" + hitsList);
 	}
 }

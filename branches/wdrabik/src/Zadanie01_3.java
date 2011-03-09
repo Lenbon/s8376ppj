a3af2f180bfcd36b214569af5343533a20d1c630
s8376 <s8376@pjwstk.edu.pl>
2011-03-09 08:49:27 +0000

    Zadanie01_3 part

--- branches/wdrabik/src/Zadanie01_3.java
+++ branches/wdrabik/src/Zadanie01_3.java
@@ -32,51 +32,83 @@ public class Zadanie01_3 {
 		Integer[] user = new Integer[6];
 		Integer[] machine = new Integer[6];
 
-		Integer inputInteger;
-		String input, message = "";
-		String[] array;
-		boolean inputFlag = false;
+//		Integer inputInteger;
+//		String input, message = "";
+//		String[] array;
+//		boolean inputFlag = false;
+//		
+//		while (inputFlag == false) {
+//			try {
+//				input = JOptionPane.showInputDialog((message.length() > 0 ? message + "\n" : "") + "Podaj ciąg znaków:");
+//				if (input == null) {
+//					System.exit(0);
+//				}
+//				array = input.split(" ");
+//			
+//				if (array.length != 6) {
+//					throw new Exception("Podaj dokładnie 6 liczb");
+//				}
+//				for (int x = 0; x < array.length; x++) {
+//					try {
+//						inputInteger = Integer.parseInt(array[x]);
+//						if (inputInteger < 1 || inputInteger > 49) {
+//							throw new Exception("Podane liczby muszą być z zakresu 1 - 49 (podano " + inputInteger + ")");	
+//						}
+//						for (Integer val : user) {
+//							if (val.equals(input)) {
+//								throw new Exception("Zdublowana liczba " + inputInteger);		
+//							}
+//						}
+//						user[x] = inputInteger;
+//					} catch (NumberFormatException e) {
+//						throw new Exception("Nieprawidłowy format danych wejściowych.\nSpróbuj: 1 2 3 4 5 6");
+//					}
+//				}
+//				
+//				inputFlag = true;
+//			} catch (Exception e) {
+//				message = e.getMessage();
+//				inputFlag = false;
+//			}
+//		}
+//		
+//		user = sort(user);
 		
-		while (inputFlag == false) {
-			try {
-				input = JOptionPane.showInputDialog((message.length() > 0 ? message + "\n" : "") + "Podaj ciąg znaków:");
-				if (input == null) {
-					System.exit(0);
+		
+		
+		System.out.println((1 + (int) Math.random() * 48));
+		System.exit(0);
+		
+		boolean randomFlag;
+		int machineItems = 0, random;
+
+		while (machineItems < 6) {
+			randomFlag = true;
+			random = (1 + (int) Math.random() * 48);
+
+			for (Integer val : user) {
+				if (val.equals(random)) {
+					randomFlag = false;
+					break;
 				}
-				array = input.split(" ");
-			
-				if (array.length != 6) {
-					throw new Exception("Podaj dokładnie 6 liczb");
-				}
-				for (int x = 0; x < array.length; x++) {
-					try {
-						inputInteger = Integer.parseInt(array[x]);
-						if (inputInteger < 1 || inputInteger > 49) {
-							throw new Exception("Podane liczby muszą być z zakresu 1 - 49 (podano " + inputInteger + ")");	
-						}
-						for (Integer val : user) {
-							if (val.equals(input)) {
-								throw new Exception("Zdublowana liczba " + inputInteger);		
-							}
-						}
-						user[x] = inputInteger;
-					} catch (NumberFormatException e) {
-						throw new Exception("Nieprawidłowy format danych wejściowych.\nSpróbuj: 1 2 3 4 5 6");
-					}
-				}
-				
-				inputFlag = true;
-			} catch (Exception e) {
-				message = e.getMessage();
-				inputFlag = false;
+			}
+			if (randomFlag == true) {
+				machine[machineItems] = random;
+				machineItems++;
 			}
 		}
+
+//		machine = sort(machine);
 		
-		user = sort(user);
-		
-		System.out.println("Liczby podane przez użytkownika:");
-		
-		for (Integer val : user) {
+//		System.out.println("Liczby podane przez użytkownika:");
+
+//		for (Integer val : user) {
+//			System.out.print(" " + val);
+//		}
+
+		System.out.println("\nLiczby podane przez użytkownika:");
+
+		for (Integer val : machine) {
 			System.out.print(" " + val);
 		}
 	}

37e43659cf60d2b9abedfc971f450a5602164696
s8376@pjwstk.edu.pl <s8376@pjwstk.edu.pl@2c044fcf-a0ce-7996-88eb-45ac795e61ee>
2011-03-15 06:19:42 +0000

    zadanie 01 2

--- branches/wdrabik/src/Zadanie01_2.java
+++ branches/wdrabik/src/Zadanie01_2.java
@@ -1,10 +1,9 @@
-import java.io.ObjectInputStream.GetField;
 import java.util.Calendar;
-import java.util.HashMap;
+import java.util.Hashtable;
 
 import javax.swing.JOptionPane;
 
-import Zadanie2.*;
+import Zadanie01_2.*;
 
 /**
  * Zadanie 2 - działanie bankomatu
@@ -24,92 +23,87 @@ import Zadanie2.*;
  */
 public class Zadanie01_2 {
 	public static void main(String[] args) {
-		try {
-			
-			Calendar calendar = Calendar.getInstance();
-			
-			System.out.println(calendar.getTime()); 
-			
-			System.exit(0);
-			
-			
-			
-			boolean inputFlag = false;
-			String input, dialogTitle = "", faceValue = "PLN";
-			String configFile = "files/Zadanie2Storage.txt";
-			String cardNum = "555123112312314"; // symulacja wlozonej karty
-			
-			int tryLeft = 3;
-			boolean pinBreakFlag = false;
-			
-			Model model = new Model(configFile);
-
-			HashMap<String, String> account = model.getAccount(cardNum);
-			
-			System.out.println(account.toString());
-			
-			while (inputFlag == false) {
-				try {
-					if (tryLeft <= 0) {
-						pinBreakFlag = true;
-						throw new Exception("Wykorzystano limit prób wpisania numeru PIN");
-					}
-					
-					input = JOptionPane.showInputDialog((dialogTitle.length() > 0 ? dialogTitle + "\n" : "") + "Wprowadź PIN (pozostały " + tryLeft + ")");
-					
-					if (account.get("pin").equals(input)) {
-						inputFlag = true;	
-					} else {
-						throw new Exception("Niepoprawny numer PIN");
-					}
-				} catch (Exception e) {
-					dialogTitle = e.getMessage();
-					tryLeft--;
-					inputFlag = false;
-					if (pinBreakFlag == true) {
-						JOptionPane.showMessageDialog(null, e.getMessage());
-						System.exit(0);
+		boolean inputFlag;
+		Calendar calendar;
+		int pinCounter = 3;
+		String input, titleMessage, faceValue = "PLN";
+		Model model = new Model("Files/Zadanie2Storage.txt");
+		
+		String cardNum = "555123112312313"; // symulacja wlozonej karty
+		
+		while (true) {
+			try {
+				Hashtable<String, String> account = model.getAccount(cardNum);
+				
+				inputFlag = false;
+				titleMessage = "";
+				while (inputFlag == false) {
+					try {
+						input = JOptionPane.showInputDialog((titleMessage.length() > 0 ? titleMessage + "\n" : "") + "Podaj numer PIN");
+						if (account.get("pin").equals(input)) {
+							inputFlag = true;
+						} else {
+							throw new Exception("Niepoprawny numer PIN");
+						}
+					} catch (Exception e) {
+						pinCounter--;
+						if (pinCounter <= 0) {
+							throw new Exception("Przekroczono limit prób podania numeru PIN");	
+						}
+						titleMessage = "Błędny PIN, pozostało prób:" + pinCounter;
+						inputFlag = false;
 					}
 				}
-			}
-			
-			inputFlag = false;
-			dialogTitle = "";
-			
-			while (inputFlag == false) {
-				try {
-					input = JOptionPane.showInputDialog((dialogTitle.length() > 0 ? dialogTitle + "\n" : "") + "Podaj kwotę do pobrania");
+				
+				inputFlag = false;
+				titleMessage = "";
+				while (inputFlag == false) {
+					try {
+						input = JOptionPane.showInputDialog((titleMessage.length() > 0 ? titleMessage + "\n" : "") + "Podaj kwotę do pobrania");
 
-					if (Double.parseDouble(input) % 100 != 0) {
-						throw new Exception("Bankomat wypłaca jedynie banknoty o niminale 100" + faceValue);
-					}
-					
-					if (Double.parseDouble(input) > Double.parseDouble(account.get("value"))) {
-						throw new Exception("Podana kwota przewyższa dostępy stan konta: " + account.get("value") + faceValue);
-					}
+						if (Double.parseDouble(input) % 100 != 0) {
+							throw new Exception("Bankomat wypłaca jedynie banknoty o niminale 100" + faceValue);
+						}
 
-					
-					
-					
-					
-					if (Double.parseDouble(input) > Double.parseDouble(account.get("limit"))){
-						throw new Exception("Podana kwota przewyższa jednorazowy limit wypłaty: " + account.get("limit") + faceValue);
+						if (Double.parseDouble(input) > Double.parseDouble(account.get("balance"))) {
+							throw new Exception("Podana kwota przewyższa dostępny stan konta: " + account.get("balance") + faceValue);
+						}
+						
+						if (Double.parseDouble(input) > Double.parseDouble(account.get("limitPerOnce"))){
+							throw new Exception("Podana kwota przewyższa jednorazowy limit wypłaty: " + account.get("limitPerOnce") + faceValue);
+						}
+						
+						if (Double.parseDouble(input) > Double.parseDouble(account.get("limitPerDay")) - Double.parseDouble(account.get("counterDay"))){
+							throw new Exception("Przekroczono dzienny limit wypłaty: " + account.get("limitPerDay") + faceValue);
+						}
+
+						inputFlag = true;
+						
+						account.put("counterDay", "" + (Double.parseDouble(account.get("counterDay")) + Double.parseDouble(input)));
+						
+						calendar = Calendar.getInstance();
+						account.put("timeLastUsage", "" + calendar.getTime());
+						
+						JOptionPane.showMessageDialog(null, "Wypłacono kwotę: " + Double.parseDouble(input) + faceValue);
+						
+						model.setAccount();
+					} catch (NumberFormatException e) {
+						titleMessage = "Nieprawidłowy format kwoty";
+						inputFlag = false;
+					} catch (Exception e) {
+						titleMessage = e.getMessage();
+						inputFlag = false;
 					}
-					
-					JOptionPane.showMessageDialog(null, "Wypłacono kwotę: " + Double.parseDouble(input) + faceValue);
-					
-					inputFlag = true;
-				} catch (NumberFormatException e) {
-					dialogTitle = "Nieprawidłowy format kwoty";
-					inputFlag = false;
-				} catch (Exception e) {
-					dialogTitle = e.getMessage();
-					inputFlag = false;
 				}
+				
+				if (JOptionPane.showConfirmDialog(null, "Czy wykonać następną operację?") >= 1) {
+					throw new Exception("Dziękujemy za skorzystanie z usług sieci.");
+				}
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(null, e.getMessage());
+				break;
 			}
-			
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null, e.getMessage());
 		}
+		System.out.println("exit");
 	}
 }

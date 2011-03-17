250b3fa86987c22028fb5a332945932208dcb665
s8376@pjwstk.edu.pl <s8376@pjwstk.edu.pl@2c044fcf-a0ce-7996-88eb-45ac795e61ee>
2011-03-17 06:19:24 +0000

    zadanie01 2 part; zadanie01 4start;

--- branches/wdrabik/src/Zadanie01_4.java
+++ branches/wdrabik/src/Zadanie01_4.java
@@ -1,4 +1,17 @@
-
+/**
+ * Zadanie 4 - gra planszowa "kółko i krzyżyk"
+ *  
+ * Napisać progam symulujący popularną grę planszową "kółko i krzyżyk" (w trybie tekstowym).
+ * Jeden z graczy to użytkownik uruchamiający program a drugi to komputer.
+ * Pola planszy w programie to komórki tablicy dwuwymiarowej o odpowiednim wymiarze.
+ * Po każdym zagraniu program prezentuje aktualne wypełnienie planszy oraz wynik gry.
+ * Program musi być napisany w sposób skalowalny - gra może odbywać się na planszy 3x3, 4x4, 5x5 itd.
+ * Trafienia każdego z graczy liczymy wzdłuż wierszy, kolumn i przekątnych planszy
+ * a ilość trafień podajemy jako parametr gry.
+ *
+ * @author s8376
+ * @version $Revision$
+ */
 public class Zadanie01_4 {
 
 }

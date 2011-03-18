f95280e7b90a5d29aad95bc8bbf426824b16ce56
s8376@pjwstk.edu.pl <s8376@pjwstk.edu.pl@2c044fcf-a0ce-7996-88eb-45ac795e61ee>
2011-03-18 19:20:57 +0000

    zadanie 01 2

--- branches/wdrabik/src/Zadanie01_2.java
+++ branches/wdrabik/src/Zadanie01_2.java
@@ -1,10 +1,6 @@
-import java.util.*;
-import static java.util.Calendar.*;
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Hashtable;
 
 import javax.swing.JOptionPane;
 
@@ -29,61 +25,67 @@ import Zadanie01_2.*;
 public class Zadanie01_2 {
 	public static void main(String[] args) {
 		boolean inputFlag;
-		
-		Date dateNow, dateModel;
-		String dateFormat = "yyyyMMddHHmm";
-		
+		boolean pinOk = false;
 		int pinCounter = 3;
 		String input, titleMessage, faceValue = "PLN";
 		Model model = new Model("Files/Zadanie2Storage.txt");
-		
+		Hashtable<String, String> account;
+
 		String cardNum = "555123112312313"; // symulacja wlozonej karty
 		
 		while (true) {
 			try {
-				Hashtable<String, String> account = model.getAccount(cardNum);
+				account = model.getAccount(cardNum);
 				
-				inputFlag = false;
-				titleMessage = "";
-				while (inputFlag == false) {
-					try {
-						input = JOptionPane.showInputDialog((titleMessage.length() > 0 ? titleMessage + "\n" : "") + "Podaj numer PIN");
-						if (account.get("pin").equals(input)) {
-							inputFlag = true;
-						} else {
-							throw new Exception("Niepoprawny numer PIN");
+				if (pinOk == false) {
+					inputFlag = false;
+					titleMessage = "";
+					while (inputFlag == false) {
+						try {
+							input = JOptionPane.showInputDialog((titleMessage.length() > 0 ? titleMessage + "\n" : "") + "Podaj numer PIN");
+							if (account.get("pin").equals(input)) {
+								inputFlag = true;
+								pinOk = true;
+							} else {
+								throw new Exception("Niepoprawny numer PIN");
+							}
+						} catch (Exception e) {
+							pinCounter--;
+							if (pinCounter <= 0) {
+								throw new Exception("Przekroczono limit prób podania numeru PIN");	
+							}
+							titleMessage = "Błędny PIN, pozostało prób:" + pinCounter;
+							inputFlag = false;
 						}
-					} catch (Exception e) {
-						pinCounter--;
-						if (pinCounter <= 0) {
-							throw new Exception("Przekroczono limit prób podania numeru PIN");	
-						}
-						titleMessage = "Błędny PIN, pozostało prób:" + pinCounter;
-						inputFlag = false;
 					}
 				}
 				
+				Date dateNow, dateModel;
+				double inputDouble;
+				String dateFormat = "yyyyMMddHHmm";
+				SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat);
+				
 				inputFlag = false;
 				titleMessage = "";
-				double inputDouble;
-				SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat);
+				
 				while (inputFlag == false) {
 					try {
-						
-						Calendar calendarNow = Calendar.getInstance();
-						Calendar calendarModel = Calendar.getInstance();
-						
-						
-						calendarModel.
-						
-						dateNow = new Date();
-						dateNow.compareTo(dateModel)
 						// RESET LICZNIKA
+						dateNow = new Date();
 						dateModel = simpleDateFormat.parse(account.get("timeLastUsage"));
-
+						
+						if (simpleDateFormat.format(dateNow).toString().substring(0, 8).equals(simpleDateFormat.format(dateModel).toString().substring(0, 8))) {
+							// do nothing
+						} else {
+							account.put("counterDay", "0");
+						}
 						
 						input = JOptionPane.showInputDialog((titleMessage.length() > 0 ? titleMessage + "\n" : "") + "Podaj kwotę do pobrania");
 
+						if (input == null) {
+							break;
+						}
+						
 						try {
 							inputDouble = Double.parseDouble(input);	
 						} catch (NumberFormatException e) {
@@ -91,7 +93,7 @@ public class Zadanie01_2 {
 						}
 						
 						if (inputDouble % 100 != 0) {
-							throw new Exception("Bankomat wypłaca jedynie banknoty o niminale 100" + faceValue);
+							throw new Exception("Bankomat wypłaca jedynie banknoty o nominale 100" + faceValue);
 						}
 
 						if (inputDouble > Double.parseDouble(account.get("balance"))) {
@@ -108,14 +110,15 @@ public class Zadanie01_2 {
 
 						inputFlag = true;
 						
+						account.put("balance", "" + (Double.parseDouble(account.get("balance")) - inputDouble));
 						account.put("counterDay", "" + (Double.parseDouble(account.get("counterDay")) + inputDouble));
 
 						StringBuilder dateStringNow = new StringBuilder(simpleDateFormat.format(dateNow));
 						account.put("timeLastUsage", dateStringNow.toString());
 
-						JOptionPane.showMessageDialog(null, "Wypłacono kwotę: " + inputDouble + faceValue);
-
 						model.setAccount();
+						
+						JOptionPane.showMessageDialog(null, "Wypłacono kwotę: " + inputDouble + faceValue);
 					} catch (Exception e) {
 						titleMessage = e.getMessage();
 						inputFlag = false;

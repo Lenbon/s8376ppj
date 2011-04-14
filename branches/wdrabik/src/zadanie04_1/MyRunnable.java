ccbd4ed237d19d808afe2b09418513cffb15f4c6
s8376@pjwstk.edu.pl <s8376@pjwstk.edu.pl@2c044fcf-a0ce-7996-88eb-45ac795e61ee>
2011-04-14 15:58:04 +0000

    Zadanie04 4 part

--- branches/wdrabik/src/zadanie04_1/MyRunnable.java
+++ branches/wdrabik/src/zadanie04_1/MyRunnable.java
@@ -1,15 +1,15 @@
 package zadanie04_1;
 
-public class MyRunnable extends Thread implements Runnable {
-	public void run() {
-		try {
-		int x = 0;
-		while (true) {
-			System.out.println(++x);
-			sleep(300);
-		}
-		} catch (InterruptedException e) {
-			return;
-		}
-	}
+public class MyRunnable implements Runnable {
+    public void run() {
+        try {
+            int x = 0;
+            while (true) {
+                System.out.println(++x);
+                Thread.sleep(300);
+            }
+        } catch (InterruptedException e) {
+            return;
+        }
+    }
 }

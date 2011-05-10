70014509bc76b3071a42b5ac81339acf435d8815
s8376 <s8376@pjwstk.edu.pl>
2011-05-10 05:58:37 +0000

    zadanie05 part

--- branches/javaguiwdrabik/src/Zadanie05_1.java
+++ branches/javaguiwdrabik/src/Zadanie05_1.java
@@ -1,57 +1,135 @@
-// Animowanie figury
-// okrag odbija sie od krawedzi pulpitu.   
-// przygotowanie wpólrzednych figury w watku glownym,
-// wykreslanie w watku zdarzeniowym w paintComponent().
-
+// Rysowanie na komponencie
+// Na komponencie mozna rysowac i wypelniac: 
+// proste figury geometryczne,napisy,obrazy (umozliwiaja to metody klasy Graphics);
+// w ciele metody paintComponent() mamy dostep do obiektu wykreslajacego klasy Graphics  
 import java.util.*;
 import java.awt.*;
 import javax.swing.*;
 
-public class Zadanie05_1 extends JPanel {
-    int dim = 20; //srednica figury       
-    int x = 75, y = 75; //polozenie poczatkowe       
-    int dx = 3, dy = 5; //dlugosc kroku w kierunku x i y       
-    int delay = 40; //opoznienie odswiezania   
+/**
+ * Zadanie 1  - Rysowanie na komponencie - wymiarowanie w pikslach
+ * 
+ * Poniższy kod programu generuje okno ramowe aplikacji i na panelu wstawionym do okna rysuje i wypełnia prostą figurę geometryczną.
+ * 
+ * // Rysowanie na komponencie
+ * // Na komponencie mozna rysowac i wypelniac: 
+ * // proste figury geometryczne,napisy,obrazy (umozliwiaja to metody klasy Graphics);
+ * // w ciele metody paintComponent() mamy dostep do obiektu wykreslajacego klasy Graphics  
+ * 
+ * import java.util.*;
+ * import java.awt.*;
+ * import javax.swing.*;
+ * 
+ * class Graph extends JPanel {
+ *  int dim = 20;  //srednica figury
+ *  int x = 75, y = 75; //polozenie             
+ *  public void paintComponent(Graphics g){
+ *      super.paintComponent(g); //wykreslanie komponentu JPanel                           
+ *      g.setColor(Color.blue); //ustawienie koloru wykreslania
+ *      g.fillOval(x, y, dim, dim); //wypelnianie kola
+ *  }
+ *  public Dimension getPreferredSize(){
+ *      return new Dimension(200, 200);
+ *  }
+ * }
+ * class Main {
+ *  public static void main(String[] args){
+ *      JFrame frame = new JFrame("Rysowanie ");//utworzenie okna ramowego
+ *      Graph graph  = new Graph(); //utworzenie panelu do rysowania
+ *      frame.getContentPane().add(graph); //dodanie panelu do okna ramowego
+ *      frame.setLocation(300, 300); //lokalizacja okna na ekranie
+ *      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //obsługa zamkniecia okna
+ *      frame.pack(); //upakowanie okna
+ *      frame.show(); //wyswietlenie okna na ekranie
+ *  }
+ * }
+ * 
+ * Uruchomić ten program i zaobserwować jego działanie (wpływ parametrów panelu i figur).
+ * 
+ * Po zapoznaniu z dokumentacją klasy Graphics:
+ * 
+ * wyrysować i ewentualnie wypełnić inne figury geometryczne o wymiarach podanych w pikslach:
+ * - linię, prostokąt, trójkąt,wycinek koła
+ * - wyrysować określony napis w odpowiednim położeniu 
+ * - wyrysować obraz z pliku graficznego
+ * 
+ * Zoabserwować czy wymiary figur zmieniają sie przy zmianach rozmiaru okienka aplikacji
+ * 
+ * @author s8376
+ * @version $Revision$
+ */
+class Graph extends JPanel {
+    int dim = 20; //srednica figury
+    int x = 75, y = 75; //polozenie             
+    Image img;
+    boolean loaded = false;
 
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.setColor(Color.blue); //ustawienie koloru wykreslania
-        g.fillOval(x, y, dim, dim); //wypełnienie kola kolorem
+    private void loadImage(String imgFileName) {
+        img = Toolkit.getDefaultToolkit().getImage(imgFileName);
+        MediaTracker mt = new MediaTracker(this);
+        mt.addImage(img, 1);
+        try {
+            mt.waitForID(1);
+        } catch (InterruptedException exc) {
+        }
+        int w = img.getWidth(this); // szerokość obrazka
+        int h = img.getHeight(this); // wysokość obrazka
+        if (w != -1 && w != 0 && h != -1 && h != 0) {
+            loaded = true;
+            setPreferredSize(new Dimension(w, h));
+        } else
+            setPreferredSize(new Dimension(200, 200));
     }
 
-    public void startAnim() {
-        while (true) {
-            // odbicie
-            if (x + dim > getWidth() || x < 0)
-                dx = -dx;
-            if (y + dim > getHeight() || y < 0)
-                dy = -dy;
-            // przesuniecie wzdluz x i y
-            x += dx;
-            y += dy;
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g); //wykreslanie komponentu JPanel                           
+        g.setColor(Color.blue); //ustawienie koloru wykreslania
+        g.fillOval(x, y, dim, dim); //wypelnianie kola
 
-            repaint(); //wstawienie paintComponent() do watku zdarzeniowego
-
-            try {
-                Thread.sleep(delay);
-            } //uspienie watku
-            catch (InterruptedException e) {
-            }
+        // wyrysować obraz z pliku graficznego
+        loadImage("files/images/zadanie05_1_1.jpg");
+        if (img != null && loaded) {
+            g.drawImage(img, 0, 0, img.getWidth(this), img.getHeight(this), this);
         }
+        
+        // wyrysować i ewentualnie wypełnić inne figury geometryczne o wymiarach podanych w pikslach
+        // linię
+        g.setColor(Color.red);
+        g.drawLine(25, 25, 175, 25);
+        // prostokąt
+        g.setColor(Color.black);
+        g.drawRect(25, 100, 25, 25);
+        g.setColor(Color.green);
+        g.fillRect(25, 100, 25, 25);
+        // trójkąt
+        int[] tabX = { 50, 75, 25 }, tabY = { 50, 25, 25 };
+        g.setColor(Color.black);
+        g.drawPolygon(tabX, tabY, 3);
+        g.setColor(Color.cyan);
+        g.fillPolygon(tabX, tabY, 3);
+        // wycinek koła
+        g.setColor(Color.gray);
+        g.fillArc(150, 150, 50, 50, 0, 255);
+        // wyrysować określony napis w odpowiednim położeniu
+        g.setColor(Color.black);
+        g.drawString("Hello world!", 26, 176);
+        g.setColor(Color.orange);
+        g.drawString("Hello world!", 25, 175);
     }
 
     public Dimension getPreferredSize() {
         return new Dimension(200, 200);
     }
+}
 
+class Zadanie05_1 {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Animacja"); //utworzenie okna ramowego
-        Zadanie05_1 anim = new Zadanie05_1(); //utworzenie panelu do rysowania
-        frame.getContentPane().add(anim); //dodanie panelu do okna ramowego
+        JFrame frame = new JFrame("Rysowanie ");//utworzenie okna ramowego
+        Graph graph = new Graph(); //utworzenie panelu do rysowania
+        frame.getContentPane().add(graph); //dodanie panelu do okna ramowego
         frame.setLocation(300, 300); //lokalizacja okna na ekranie
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //obsługa zamkniecia okna
         frame.pack(); //upakowanie okna
-        frame.show(); //wyswietlenie okna na ekranie       
-        anim.startAnim(); // rozpoczecie animacji
+        frame.show(); //wyswietlenie okna na ekranie        
     }
 }

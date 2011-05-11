5fb4e16ba1f51122bb7006f8e2e4eeb213a82a00
s8376@pjwstk.edu.pl <s8376@pjwstk.edu.pl@2c044fcf-a0ce-7996-88eb-45ac795e61ee>
2011-05-11 07:19:59 +0000

    Zadanie05_3

--- branches/javaguiwdrabik/src/zadanie05_3/Graph.java
+++ branches/javaguiwdrabik/src/zadanie05_3/Graph.java
@@ -2,12 +2,10 @@ package zadanie05_3;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.MediaTracker;
-import java.awt.Shape;
-import java.awt.Stroke;
 import java.awt.Toolkit;
 
 import javax.swing.JPanel;
@@ -15,27 +13,42 @@ import javax.swing.JPanel;
 public class Graph extends JPanel {
     Image img;
     boolean loaded = false;
+    int outerBorder, border;
 
     public Graph(String fileName) {
         loadImage(fileName);
-        setPreferredSize(new Dimension(img.getWidth(this) + img.getHeight(this)
-                / 2, img.getHeight(this) + img.getHeight(this) / 2));
+
+        if (img != null && loaded) {
+            outerBorder = img.getWidth(this) / 10;
+            border = img.getHeight(this) / 4;
+
+            if (outerBorder < 1) {
+                outerBorder = 1;
+            }
+            if (border < 1) {
+                border = 1;
+            }
+
+            setPreferredSize(new Dimension(img.getWidth(this)
+                    + (border + outerBorder) * 2, img.getHeight(this)
+                    + (border + outerBorder) * 2));
+        }
     }
 
     private void loadImage(String imgFileName) {
         img = Toolkit.getDefaultToolkit().getImage(imgFileName);
-        
+
         MediaTracker mt = new MediaTracker(this);
         mt.addImage(img, 1);
-        
+
         try {
             mt.waitForID(1);
         } catch (InterruptedException exc) {
         }
-        
+
         int w = img.getWidth(this); // szerokość obrazka
         int h = img.getHeight(this); // wysokość obrazka
-        
+
         if (w != -1 && w != 0 && h != -1 && h != 0) {
             loaded = true;
         } else {
@@ -43,30 +56,30 @@ public class Graph extends JPanel {
         }
     }
 
-    public void paintComponent(Graphics gs) {
-        super.paintComponent(gs);
-        
-        Graphics2D g = (Graphics2D) gs;
-        
-        
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
         if (img != null && loaded) {
-            g.drawImage(img, img.getWidth(this) / 4, img.getHeight(this) / 4,
-                    img.getWidth(this), img.getHeight(this), this);
-
-            int brushSize = img.getWidth(this) / 10;
-            if (brushSize <= 0) {
-                brushSize = 1;
-            }
-
-            
-//            g.setStroke(TWO_PIXEL_STROKE);
+            // ramka
             g.setColor(Color.black);
-            g.drawRect(0, 0, img.getWidth(this) + img.getWidth(this) / 2 - brushSize, img.getHeight(this) + img.getHeight(this) / 2 - brushSize);
-            
-            String imageTitle = "Hello world!";
-            int imageTitleX = this.getWidth() / 2 - g.getFontMetrics().stringWidth(imageTitle) / 2,
-                imageTitleY = 10;
+            g.fillRect(0, 0, getWidth(), getHeight());
 
+            g.setColor(Color.white);
+            g.fillRect(outerBorder, outerBorder, getWidth() - outerBorder * 2,
+                    getHeight() - outerBorder * 2);
+
+            g.drawImage(img, outerBorder + border, outerBorder + border, img
+                    .getWidth(this), img.getHeight(this), this);
+
+            // napis
+            String imageTitle = "Hello world!";
+
+            int imageTitleX, imageTitleY;
+            imageTitleX = this.getWidth() / 2
+                    - g.getFontMetrics().stringWidth(imageTitle) / 2;
+            imageTitleY = img.getHeight(this) + outerBorder + border + (border / 2);
+
+            g.setFont(new Font("Sanserif", Font.ITALIC, 13));
             g.setColor(Color.gray);
             g.drawString(imageTitle, imageTitleX + 1, imageTitleY + 1);
             g.setColor(Color.black);

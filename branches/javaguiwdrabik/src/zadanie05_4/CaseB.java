54724c99979d5b20c086bfb1e5be7c9071be36e3
s8376 <s8376@pjwstk.edu.pl>
2011-05-12 16:02:29 +0000

    Zadanie05_4 part

--- branches/javaguiwdrabik/src/zadanie05_4/CaseB.java
+++ branches/javaguiwdrabik/src/zadanie05_4/CaseB.java
@@ -1,26 +1,61 @@
 package zadanie05_4;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 public class CaseB extends Abstract {
-    public void run() {
+    {
+        color = Color.red;
         x = dim / 2;
-        y = dim / 2;     
+        y = dim / 2;
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.red);
+        g.fillOval(x, y, dim, dim);
+    }
+
+    public void run() {
+        String direct = "right";
+        String lastDirect = "left";
+        
 
         while (true) {
             // odbicie
-            if (x + dim > getWidth() || x < 0)
-                dx = -dx;
-            if (y + dim > getHeight() || y < 0)
-                dy = -dy;
-            // przesuniecie wzdluz x i y
-            x += dx;
-            y += dy;
-
+            if (direct.equals("left")) {
+                if (x - dim < getWidth() || x < 0) {
+                    lastDirect = "right";
+                    direct = "down";
+                }
+            } else if (direct.equals("right")) {
+                if (x + dim > getWidth() || x < 0) {
+                    lastDirect = "left";
+                    direct = "down";
+                }
+            } else if (direct.equals("down")) {
+                if (y + dim > getHeight()) {
+                    return;
+                }
+                direct = lastDirect;
+            }
+            
+            // przesuniecie
+            if (direct.equals("left")) {
+                x -= dx;
+            } else if (direct.equals("right")) {
+                x += dx;
+            } else if (direct.equals("down")) {
+                y += dy;
+            }
+            
+            
+            
             repaint();
 
             try {
                 Thread.sleep(delay);
-            }
-            catch (InterruptedException e) {
+            } catch (InterruptedException e) {
             }
         }
     }

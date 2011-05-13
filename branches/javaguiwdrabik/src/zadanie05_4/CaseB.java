1c435e0dc965ee445abe1251f6d065efd2b74a53
s8376@pjwstk.edu.pl <s8376@pjwstk.edu.pl@2c044fcf-a0ce-7996-88eb-45ac795e61ee>
2011-05-13 12:04:49 +0000

    Zadanie05_4 part

--- branches/javaguiwdrabik/src/zadanie05_4/CaseB.java
+++ branches/javaguiwdrabik/src/zadanie05_4/CaseB.java
@@ -3,53 +3,15 @@ package zadanie05_4;
 import java.awt.Color;
 
 public class CaseB extends Abstract {
-    int vDistance = 30;
     {
+        eGauge = getWidth();
+        sGauge = 30;
+
         color = Color.green;
 
         x = dim / 2;
         y = dim / 2;
-    }
 
-    public void run() {
-        // l left, r right, v vertically
-        char direction = 'r';
-        char lastDirection = 'v';
-        int directionChangeInY = y;
-        
-        while (true) {
-            if (direction == 'r') {
-                x += dx;
-                if (x + dim > getWidth()) {
-                    lastDirection = direction;
-                    directionChangeInY = y;
-                    direction = 'v';
-                }
-            } else if (direction == 'l') {
-                x -= dx;
-                if (x - dim < 0) {
-                    lastDirection = direction;
-                    directionChangeInY = y;
-                    direction = 'v';
-                }
-            } else if (direction == 'v') {
-                y += dy;
-                if (y + dim > getHeight()) {
-                    // koniec obszaru, koncze rysowanie
-                    return;
-                }
-                if (y + dy > directionChangeInY + vDistance) {
-                    direction = lastDirection == 'l' ? 'r' : 'l';
-                    lastDirection = 'v';
-                }
-            }
-
-            repaint();
-
-            try {
-                Thread.sleep(delay);
-            } catch (InterruptedException e) {
-            }
-        }
+        directionInit = 'e';
     }
 }

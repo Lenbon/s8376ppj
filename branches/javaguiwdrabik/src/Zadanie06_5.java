da41b18f49dcfcd84c342f231989389aa4efb663
s8376 <s8376@pjwstk.edu.pl>
2011-05-14 22:53:13 +0000

    Zadanie06_5

--- branches/javaguiwdrabik/src/Zadanie06_5.java
+++ branches/javaguiwdrabik/src/Zadanie06_5.java
@@ -1,16 +1,11 @@
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.GridLayout;
-import java.io.ObjectInputStream.GetField;
 
 import net.miginfocom.swing.*;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 

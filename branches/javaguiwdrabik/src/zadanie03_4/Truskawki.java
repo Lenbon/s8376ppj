991a43b27172b026451cf57a07befc314c6067c7
s8376@pjwstk.edu.pl <s8376@pjwstk.edu.pl@2c044fcf-a0ce-7996-88eb-45ac795e61ee>
2011-05-22 11:30:35 +0000

    cleaning

--- branches/javaguiwdrabik/src/zadanie03_4/Truskawki.java
+++
@@ -1,7 +0,0 @@
-package zadanie03_4;
-
-public class Truskawki extends Product {
-    public Truskawki(double quantity) {
-        super("Truskawki", quantity);
-    }
-}
--- branches/javaguiwdrabik/src/zadanie03_6/Mandarynki.java
+++
@@ -1,7 +0,0 @@
-package zadanie03_6;
-
-public class Mandarynki extends Product implements Owoce {
-    public Mandarynki(double quantity) {
-        super("Mandarynki", quantity);
-    }
-}

356b83b7846d84d3afbdc75b60726748316c2e84
s8376@pjwstk.edu.pl <s8376@pjwstk.edu.pl@2c044fcf-a0ce-7996-88eb-45ac795e61ee>
2011-05-22 20:50:04 +0000

    zadanie06_3 final

--- branches/javaguiwdrabik/src/Zadanie06_3.java
+++ branches/javaguiwdrabik/src/Zadanie06_3.java
@@ -1,3 +1,11 @@
+import java.awt.Dimension;
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+
+import zadanie06_3.*;
+
 /**
  * Zad. 3.  Obrazki (max. 10p)
  * 
@@ -11,5 +19,32 @@
  * @version $Id$
  */
 public class Zadanie06_3 {
+    public static void main(String[] args) {
+        try {
+            String path = "files/zadanie06_3/";
 
+            JFrame frame = new JFrame();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setLocation(300, 300);
+            frame.setPreferredSize(new Dimension(210, 155));
+
+            ArrayList<File> list = new ArrayList<File>();
+
+            File myFile = new File(path);
+            for (File file : myFile.listFiles()) {
+                list.add(file);
+            }
+
+            Panel panel = new Panel(path, list);
+            frame.add(panel);
+            Thread thread = new Thread(panel);
+
+            frame.pack();
+            frame.setVisible(true);
+
+            thread.start();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
 }

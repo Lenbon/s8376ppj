8de8a1c08f13a806b006bce8849f8eaa8747beaf
s8376@pjwstk.edu.pl <s8376@pjwstk.edu.pl@2c044fcf-a0ce-7996-88eb-45ac795e61ee>
2011-05-28 19:30:55 +0000

    kbarteczko start and files

---
+++ branches/javaguikbarteczko/src/visual/ImagePanelTest.java
@@ -0,0 +1,57 @@
+package visual;
+
+import javax.swing.*;
+import java.awt.*;
+
+class ImagePanel extends JPanel {
+
+    Image img;
+    boolean loaded = false; // czy obrazek został załadowany?
+
+    public ImagePanel(String imgFileName) {
+        loadImage(imgFileName);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (img != null && loaded)
+            g.drawImage(img, 0, 0, getWidth(), getHeight(), this);
+        else
+            g.drawString("Bez obrazka", 10, getHeight() - 10);
+    }
+
+    private void loadImage(String imgFileName) {
+        img = Toolkit.getDefaultToolkit().getImage(imgFileName);
+        MediaTracker mt = new MediaTracker(this);
+        mt.addImage(img, 1);
+        try {
+            mt.waitForID(1);
+        } catch (InterruptedException exc) {
+        }
+        int w = img.getWidth(this); // szerokość obrazka
+        int h = img.getHeight(this); // wysokość obrazka
+        if (w != -1 && w != 0 && h != -1 && h != 0) {
+            loaded = true;
+            setPreferredSize(new Dimension(w, h));
+        } else
+            setPreferredSize(new Dimension(200, 200));
+    }
+
+}
+
+public class ImagePanelTest extends JFrame {
+
+    public ImagePanelTest(String fname) {
+        ImagePanel p = new ImagePanel(fname);
+        p.add(new JButton("Jakiś przycisk"));
+        add(p);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String args[]) {
+        new ImagePanelTest("images/intro1.jpg"); // argument - nazwa pliku graficznego (z
+        // roboczego katalogu aplikacji)
+    }
+}

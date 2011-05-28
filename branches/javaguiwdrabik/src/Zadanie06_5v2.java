7db46aa68e5d52bd5fc7a72d31349a773b6e88a2
s8376 <s8376@pjwstk.edu.pl>
2011-05-28 05:14:34 +0000

    zadanie06 5v2 part

--- branches/javaguiwdrabik/src/Zadanie06_5v2.java
+++ branches/javaguiwdrabik/src/Zadanie06_5v2.java
@@ -1,9 +1,12 @@
+import java.awt.Color;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -28,61 +31,72 @@ import javax.swing.JTextField;
  */
 public class Zadanie06_5v2 {
     public static void main(String[] args) {
-        
+
         //(02:57:22 PM) <font size="3">Jacek Głogosz: całość na gridlayout (02:57:30 PM) Jacek Głogosz: u góry 2x flowlayout (02:57:41 PM) Jacek Głogosz: na dole po lewej gridlayout (02:57:54 PM) Jacek Głogosz: (całośc na gridbag layout, a nie grid) (02:57:34 PM) Filip: uuuuuu (02:57:41 PM) Filip: zmienia to postać rzeczy :D (02:58:54 PM) Jacek Głogosz: i po prawej na dole, też gridlayout (02:59:03 PM) Jacek Głogosz: wszystko oczywiście ma dodatkowe ustawienia :)</font>
-        
+
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setPreferredSize(new Dimension(400, 350));
-        frame.setLocation(1600, 200);
-//        frame.setLayout(new GridBagLayout());
-        frame.setLayout(new GridLayout(3, 3));
-        
+        frame.setPreferredSize(new Dimension(400, 400));
+        frame.setLocation(300, 200);
+        frame.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
-        
-        // gorny panel
-        JPanel top = new JPanel();
 
+        // panel gorny
         JPanel topLeft = new JPanel();
+        
+topLeft.setBackground(Color.GREEN);
+        
         topLeft.setLayout(new FlowLayout(FlowLayout.LEFT));
         topLeft.add(new JButton("A1"));
         topLeft.add(new JButton("A2"));
         topLeft.add(new JButton("A3"));
-        top.add(topLeft);
+        c.gridx = 1;
+        c.gridy = 1;
+        frame.add(topLeft, c);
 
         JPanel topRight = new JPanel();
+        
+topRight.setBackground(Color.RED);
+        
         topRight.setLayout(new FlowLayout(FlowLayout.RIGHT));
         topRight.add(new JButton("B1"));
         topRight.add(new JButton("B2"));
         topRight.add(new JButton("B3"));
-        top.add(topRight);
+        c.gridx = 2;
+        c.gridy = 1;
+        frame.add(topRight, c);
 
-        frame.add(top);
-
-        // srodkowy panel
+        // panel srodkowy
         JPanel middle = new JPanel();
-        middle.add(new JTextArea());
+        JTextArea textarea = new JTextArea();
+        textarea.setPreferredSize(new Dimension(200, 200));
+        middle.add(textarea);
+middle.setBackground(Color.gray);
+      c.gridx = 1;
+      c.gridwidth = 2;
+      c.gridy = 2;
+        frame.add(middle, c);
 
-        frame.add(middle);
-
-        // dolny panel
-        JPanel bottom = new JPanel();
-
+        // panel dolny
         JPanel bottomLeft = new JPanel();
+bottomLeft.setBackground(Color.pink);
         bottomLeft.setLayout(new GridLayout(0, 3));
         for (int x = 0; x < 9; x++) {
             bottomLeft.add(new JButton("" + (x + 1)));
         }
-        bottom.add(bottomLeft);
+        c.gridx = 1;
+        c.gridy = 3;
+        frame.add(bottomLeft, c);
 
         JPanel bottomRight = new JPanel();
-        bottomRight.setLayout(new GridLayout(0, 1));
+bottomRight.setBackground(Color.blue);
+        bottomRight.setLayout(new BoxLayout(bottomRight, BoxLayout.Y_AXIS));
         bottomRight.add(new JTextField());
         bottomRight.add(new JTextField());
         bottomRight.add(new JTextField());
-        bottom.add(bottomRight);
-
-        frame.add(bottom);
+        c.gridx = 2;
+        c.gridy = 3;
+        frame.add(bottomRight, c);
 
         frame.pack();
         frame.setVisible(true);

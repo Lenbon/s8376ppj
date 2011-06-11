107c5dce1859e96c6811a9b4d4b30072fa9f552d
s8376 <s8376@pjwstk.edu.pl>
2011-06-11 04:44:06 +0000

    zadanie sop part

--- src/Filter.java
+++ src/Filter.java
@@ -1,5 +1,4 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Locale;
 
 /**
  * @author s8376
@@ -8,22 +7,44 @@ import java.io.InputStreamReader;
 public class Filter {
     public static void main(String[] args) {
         try {
-            String input;
+            String stringToFilter;
             try {
-                input = args[0];
+                stringToFilter = args[0];
             } catch (Exception e) {
                 throw new Exception("Nie podano ciagu znakow");
             }
-            System.out.println(input);
 
-            String mode1 = new String();
-            String mode2 = new String();
+            String input;
+            
+            // zmienna srodowiskowa
+            boolean modeReplacement = false;
+            input = System.getenv("mode1");
+            if (input != null && !input.equals("")) {
+                modeReplacement = true;
+            }
 
-            mode1 = System.getProperty("mode1");
-            mode2 = System.getenv("mode2");
+            // wlasciwosc systemowa
+            boolean modeToUpper = false;
+            input = System.getProperty("mode2");
+            if (input != null && !input.equals("")) {
+                modeToUpper = true;
+            }
+            
+            if (!modeToUpper && !modeReplacement) {
+                throw new Exception("Nie ustalono zadnego trybu");
+            }
 
-            System.out.println(mode1);
-            System.out.println(mode2);
+            // spacje na znak nowej linii / #
+            String replacement = modeReplacement ? "\n" : "_";
+            stringToFilter = stringToFilter.replaceAll(" ", replacement);
+            
+            // litery na wielkie
+            if (modeToUpper) {
+                stringToFilter = stringToFilter
+                        .toUpperCase(Locale.getDefault());
+            }
+
+            System.out.println(stringToFilter);
         } catch (Exception e) {
             System.err.println(e.toString());
             System.exit(1);

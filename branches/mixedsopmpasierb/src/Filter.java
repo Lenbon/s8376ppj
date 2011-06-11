5542cf2069b35a0fe9d8368356ac8f64488d5805
s8376 <s8376@pjwstk.edu.pl>
2011-06-11 03:30:20 +0000

    zadanie sop part

--- branches/mixedsopmpasierb/src/Filter.java
+++ branches/mixedsopmpasierb/src/Filter.java
@@ -7,16 +7,26 @@ import java.io.InputStreamReader;
  */
 public class Filter {
     public static void main(String[] args) {
-
-        // modyfikacja wc
-        // tryby wc -w
-
         try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(
-                    System.in));
-            System.out.println(br.readLine() + " ssss");
+            String input;
+            try {
+                input = args[0];
+            } catch (Exception e) {
+                throw new Exception("Nie podano ciagu znakow");
+            }
+            System.out.println(input);
+
+            String mode1 = new String();
+            String mode2 = new String();
+
+            mode1 = System.getProperty("mode1");
+            mode2 = System.getenv("mode2");
+
+            System.out.println(mode1);
+            System.out.println(mode2);
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.err.println(e.toString());
+            System.exit(1);
         }
     }
 }

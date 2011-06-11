9f1fddd5487d6d121e397fe17dc1a2a381bfca8c
s8376@pjwstk.edu.pl <s8376@pjwstk.edu.pl@2c044fcf-a0ce-7996-88eb-45ac795e61ee>
2011-06-11 05:55:42 +0000

    zad sop part

--- branches/mixedsopmpasierb/src/sop.sh
+++ branches/mixedsopmpasierb/src/sop.sh
@@ -6,6 +6,13 @@ read -p "Ciag znakow: " string
 
 export mode1="$mode1"
 
-javac Filter.java
-java -Dmode2="$mode2" Filter "$string"
+if [ -a `which java` ]; then
+	run=`which java`
+elif [ $JAVA_HOME != "" ]; then
+	run=$JAVA_HOME
+else
+	read -p "Podaj scieke do java: " run
+fi;
 
+javac Filter.java
+$run -Dmode2="$mode2" Filter "$string"

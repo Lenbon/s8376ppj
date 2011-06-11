68037b311f2cdfe8f4e9faa0c9d2da1e54b4ad35
s8376@pjwstk.edu.pl <s8376@pjwstk.edu.pl@2c044fcf-a0ce-7996-88eb-45ac795e61ee>
2011-06-11 04:44:06 +0000

    zadanie sop part

--- branches/mixedsopmpasierb/src/sop.sh
+++ branches/mixedsopmpasierb/src/sop.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-echo "Hello world!"
+
+read -p "Tryb1 - nowa linia / znak (t/f): " mode1
+read -p "Tryb2 - wielkie litery (t/f): " mode2
 
 
-read -p "Tryb1: " mode1
-read -p "Tryb2: " mode2
 
-read -p "Ciag znakow: " string
+#read -p "Ciag znakow: " string
+string="Ala ma Kota"
 
-export mode2="$mode2"
+export mode1="$mode1"
 
 javac Filter.java
-java -Dmode1="$mode1" Filter $string
+java -Dmode2="$mode2" Filter "$string"
 

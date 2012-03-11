ac500e8be064fe582533c889d19409cd3ad50e1d
s8376@pjwstk.edu.pl <s8376@pjwstk.edu.pl@2c044fcf-a0ce-7996-88eb-45ac795e61ee>
2012-03-11 19:06:50 +0000

    refactoring

--- branches/javagamestutorial/src/breakout/Sprite.java
+++ branches/javagamestutorial/src/breakout/Sprite.java
@@ -3,43 +3,52 @@ package breakout;
 import java.awt.Image;
 import java.awt.Rectangle;
 
-public class Sprite {
+public class Sprite
+{
 
-    protected int x;
-    protected int y;
-    protected int width;
-    protected int heigth;
-    protected Image image;
+	protected int x;
+	protected int y;
+	protected int width;
+	protected int heigth;
+	protected Image image;
 
-    public void setX(int x) {
-        this.x = x;
-    }
+	public void setX(int x)
+	{
+		this.x = x;
+	}
 
-    public int getX() {
-        return x;
-    }
+	public int getX()
+	{
+		return x;
+	}
 
-    public void setY(int y) {
-        this.y = y;
-    }
+	public void setY(int y)
+	{
+		this.y = y;
+	}
 
-    public int getY() {
-        return y;
-    }
+	public int getY()
+	{
+		return y;
+	}
 
-    public int getWidth() {
-        return width;
-    }
+	public int getWidth()
+	{
+		return width;
+	}
 
-    public int getHeight() {
-        return heigth;
-    }
+	public int getHeight()
+	{
+		return heigth;
+	}
 
-    Image getImage() {
-        return image;
-    }
+	Image getImage()
+	{
+		return image;
+	}
 
-    Rectangle getRect() {
-        return new Rectangle(x, y, image.getWidth(null), image.getHeight(null));
-    }
+	Rectangle getRect()
+	{
+		return new Rectangle(x, y, image.getWidth(null), image.getHeight(null));
+	}
 }
--- branches/javagamestutorial/src/collision/Collision.java
+++ branches/javagamestutorial/src/collision/Collision.java
@@ -2,19 +2,21 @@ package collision;
 
 import javax.swing.JFrame;
 
-public class Collision extends JFrame {
-    public Collision() {
-        add(new Board());
+public class Collision extends JFrame
+{
+	public Collision() {
+		add(new Board());
 
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400, 300);
-        setLocationRelativeTo(null);
-        setTitle("Collision");
-        setResizable(false);
-        setVisible(true);
-    }
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(400, 300);
+		setLocationRelativeTo(null);
+		setTitle("Collision");
+		setResizable(false);
+		setVisible(true);
+	}
 
-    public static void main(String[] args) {
-        new Collision();
-    }
+	public static void main(String[] args)
+	{
+		new Collision();
+	}
 }

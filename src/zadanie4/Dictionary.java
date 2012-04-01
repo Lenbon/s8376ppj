2e1a21a5d9ababd2d46edfa141cb5e8d240bc526
s8376 <s8376@pjwstk.edu.pl>
2012-04-01 08:58:09 +0000

    zadanie 4 rc

--- src/zadanie4/Dictionary.java
+++ src/zadanie4/Dictionary.java
@@ -6,8 +6,8 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -20,6 +20,20 @@ public class Dictionary
 {
     private String _path;
     private Map<String, List<String>> _collection;
+
+    /**
+     * Dodaje do slownika nowe haslo wraz z pierwsza definicja
+     * 
+     * @param key
+     * @param value
+     * @return void
+     */
+    private void _addNew(String key, String value)
+    {
+        ArrayList<String> newValue = new ArrayList<String>();
+        newValue.add(value);
+        _collection.put(key, newValue);
+    }
 
     /**
      * Konstruktor
@@ -55,9 +69,7 @@ public class Dictionary
                 if (_collection.containsKey(key)) {
                     _collection.get(key).add(value);
                 } else {
-                    ArrayList<String> newValue = new ArrayList<String>();
-                    newValue.add(value);
-                    _collection.put(key, newValue);
+                    _addNew(key, value);
                 }
             } catch (Exception e) {
                 //System.out.println("skipping");
@@ -65,6 +77,10 @@ public class Dictionary
         }
 
         br.close();
+
+        for (String string : _collection.keySet()) {
+            Collections.sort(_collection.get(string));
+        }
     }
 
     /**
@@ -77,13 +93,12 @@ public class Dictionary
     public void lookup(String key)
     {
         if (_collection.containsKey(key)) {
-            int no = 0;
-            
+            List<String> collection = _collection.get(key);
 
-// TODO sortowanie
+            System.out.println(key + ":");
             
-            
-            for (String string : _collection.get(key)) {
+            int no = 0;
+            for (String string : collection) {
                 System.out.println((++no) + ". " + string);
             }
         } else {
@@ -95,11 +110,23 @@ public class Dictionary
      * Dodaje do słownika hasło z definicją (o ile nie było ich wcześniej
      * w słowniku).
      * 
+     * @param key
+     * @param value
      * @return void
      */
-    public void add()
+    public void add(String key, String value)
     {
-
+        if (_collection.containsKey(key)) {
+            if (_collection.get(key).contains(value)) {
+                System.out.println("Słownik zawiera już tę definicję: " + key
+                        + " - " + value);
+            } else {
+                _collection.get(key).add(value);
+                Collections.sort(_collection.get(key));
+            }
+        } else {
+            _addNew(key, value);
+        }
     }
 
     /**
@@ -108,18 +135,35 @@ public class Dictionary
      * 
      * @return void
      */
-    public void delete()
+    public void delete(String key, int index)
     {
-
+        if (_collection.containsKey(key)) {
+            if (_collection.get(key).size() <= 1) {
+                _collection.remove(key);
+            } else {
+                _collection.get(key).remove(index);
+            }
+        } else {
+            System.out.println("Słownik nie zawiera hasła: " + key);
+        }
     }
 
     /**
      * Aktualizuje słownik, zamieniając starą definicję na nową dla podanego 
      * hasła.
+     * 
+     * @param key
+     * @param index
+     * @param definiton
+     * @return void
      */
-    public void update()
+    public void update(String key, int index, String definiton)
     {
-
+        if (_collection.containsKey(key)) {
+            _collection.get(key).set(index, definiton);
+        } else {
+            System.out.println("Słownik nie zawiera hasła: " + key);
+        }
     }
 
     /**
@@ -131,13 +175,14 @@ public class Dictionary
     public void save() throws IOException
     {
         FileWriter fw = new FileWriter(_path + "BKP");
+//        FileWriter fw = new FileWriter(_path);
         BufferedWriter bw = new BufferedWriter(fw);
 
         for (String key : _collection.keySet()) {
             for (String value : _collection.get(key)) {
                 bw.write(key + " = " + value);
                 bw.newLine();
-            }    
+            }
         }
 
         bw.close();

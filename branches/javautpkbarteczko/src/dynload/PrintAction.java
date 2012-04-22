40a55b09c007f29d3b4375b29f3480a30884223d
s8376 <s8376@pjwstk.edu.pl>
2012-04-22 20:02:55 +0000

    reflection

---
+++ branches/javautpkbarteczko/src/dynload/PrintAction.java
@@ -0,0 +1,21 @@
+package dynload;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class PrintAction extends AbstractAction
+{
+
+    final static String ACTION_NAME = "Print";
+
+    public PrintAction()
+    {
+        super(ACTION_NAME);
+    }
+
+    public void actionPerformed(ActionEvent e)
+    {
+        System.out.println("Wykonan akcja: " + ACTION_NAME);
+    }
+
+}

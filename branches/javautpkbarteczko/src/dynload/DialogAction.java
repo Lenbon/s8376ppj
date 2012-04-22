034722109eef2d9f263abbcb2268d82395851d47
s8376@pjwstk.edu.pl <s8376@pjwstk.edu.pl@2c044fcf-a0ce-7996-88eb-45ac795e61ee>
2012-04-22 20:02:55 +0000

    reflection

---
+++ branches/javautpkbarteczko/src/dynload/DialogAction.java
@@ -0,0 +1,21 @@
+package dynload;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class DialogAction extends AbstractAction
+{
+
+    final static String ACTION_NAME = "Show msg";
+
+    public DialogAction()
+    {
+        super(ACTION_NAME);
+    }
+
+    public void actionPerformed(ActionEvent e)
+    {
+        JOptionPane.showMessageDialog(null, ACTION_NAME);
+    }
+
+}

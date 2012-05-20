6a5b66efdc6fa79d6b41606a0837fe4302ed2131
s8376 <s8376@pjwstk.edu.pl>
2012-05-20 12:29:00 +0000

    zadanie 13, 14

--- branches/javautplanletruong/src/zadanie13/Location.java
+++ branches/javautplanletruong/src/zadanie13/Location.java
@@ -6,20 +6,31 @@ import java.awt.GridLayout;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.TimeZone;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Location extends JPanel
 {
-    private final Locale _locale;
+    private final DateFormat _dateFormatDate;
+    private final DateFormat _dateFormatTime;
     private final JLabel _date;
     private final JLabel _name;
     private final JLabel _time;
 
-    public Location(String locationName, String language, String country)
+    public Location(String locationName, String language, String country,
+            String timeZoneName)
     {
-        _locale = new Locale(language, country);
+        Locale locale = new Locale(language, country);
+        TimeZone timeZone = TimeZone.getTimeZone(timeZoneName);
+
+        _dateFormatDate = DateFormat.getDateInstance(DateFormat.LONG, locale);
+        _dateFormatDate.setTimeZone(timeZone);
+
+        _dateFormatTime = DateFormat
+                .getTimeInstance(DateFormat.DEFAULT, locale);
+        _dateFormatTime.setTimeZone(timeZone);
 
         setPreferredSize(new Dimension(200, 200));
         setLayout(new GridLayout(3, 1));
@@ -36,14 +47,12 @@ public class Location extends JPanel
 
     public String getDate()
     {
-        return DateFormat.getDateInstance(DateFormat.LONG, _locale).format(
-                new Date());
+        return _dateFormatDate.format(new Date());
     }
 
     public String getTime()
     {
-        return DateFormat.getTimeInstance(DateFormat.DEFAULT, _locale).format(
-                new Date());
+        return _dateFormatTime.format(new Date());
     }
 
     @Override
@@ -54,5 +63,4 @@ public class Location extends JPanel
         _date.setText(getDate());
         _time.setText(getTime());
     }
-
 }
